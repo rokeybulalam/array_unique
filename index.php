b6ae3343db58ec887aea799e0884dd9bf66b91f4
rokeybulalam <rokeybulalam1122@gmail.com>
2023-06-18 22:41:56 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// $array= [
+//     1,2,20,10,10,30,20
+// ];
+
+// $filtered_array= array_unique($array);
+
+// var_dump($filtered_array);
+
+// $array= [
+//          1,2,20,10,10,30,20
+// ];
+
+// $filtered_array = array_unique($array);
+
+// echo count($array);
+
+$array= [
+    1,2,20,10,10,30,20
+];
+
+$filtered_array = array_unique($array);
+
+echo count($filtered_array);
